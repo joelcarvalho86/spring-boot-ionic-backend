5dec6e9e027661bd7b1254ecd7d91ddcd221efd7
Joel Carvalho <jol.joel@gmail.com>
2021-01-11 12:56:45 -0300

    Paginação com Paramentros opcionais com requisição

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -58,6 +58,11 @@ public class CursomcApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e Banho");
+		Categoria cat4 = new Categoria(null, "Eletronicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		// TODO Auto-generated method stub
 		Produto p1 = new Produto(null, "Computador", 2000.00);
@@ -71,7 +76,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	
 		Estado est1 = new Estado(null, "Piauí");
