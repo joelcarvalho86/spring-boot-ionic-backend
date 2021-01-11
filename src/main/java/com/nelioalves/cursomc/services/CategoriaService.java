972e5a84397630bcaeddc9d491029cbe940cb1ae
Joel Carvalho <jol.joel@gmail.com>
2021-01-10 21:35:43 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,10 +1,13 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -12,11 +15,30 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
+
+//	public Categoria find(Integer id) {
+//		 Categoria obj = repo.findById(id).orElse(null);
+//		if (obj == null) {
+//		 throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+//		 + ", Tipo: " + Categoria.class.getName());
+//		 }
+//		return obj;
+//		}
 	public Categoria find(Integer id) {
-		return this.repo.findById(id).orElse(null);
-		}
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		} 
+	
+//	public Categoria find(Integer id) {
+//		return this.repo.findById(id).orElse(null);
+//		}
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+		}
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 		}
 
