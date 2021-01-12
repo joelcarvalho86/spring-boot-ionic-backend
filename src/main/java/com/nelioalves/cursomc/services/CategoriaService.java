77f234d329243a070eb013f8340bf7cd09b531eb
Joel Carvalho <jol.joel@gmail.com>
2021-01-12 14:31:12 -0300

    Correções: update Categoria service e tipo enumerado de pagamento

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -43,9 +43,14 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 		}
+//	public Categoria update(Categoria obj) {
+//		find(obj.getId());
+//		return repo.save(obj);
+//		}
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj); 
+		return repo.save(newObj);
 		}
 	public void delete(Integer id) {
 		find(id);
@@ -64,5 +69,9 @@ public class CategoriaService {
 	}
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria (objDto.getId(), objDto.getNome());
+	}
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		//newObj.setEmail(obj.getEmail());
 	}
 }
